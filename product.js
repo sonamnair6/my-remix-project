30e66cb2d9a855190668d55546dd0fc63222de61
sonamnair6 <135902040+sonamnair6@users.noreply.github.com>
2024-08-04 19:25:42 +1200

    Update KavaContract and associated HTML, CSS, and JS files for Kava marketplace project y y

--- product.js
+++ product.js
@@ -1,6 +1,107 @@
 async function fetchProducts() {
     const contractAddress = '0xCD4c773a5BA577075A3758499F22E5fBD55fDC6A';
-const contractABI = [
+    const contractABI = [/*... ABI from the contract above ...*/];
+    
+    if (typeof window.ethereum !== 'undefined') {
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+        
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const products = await contract.methods.getCart().call({ from: window.ethereum.selectedAddress });
+        
+        const productsDiv = document.getElementById('products');
+        products.forEach(product => {
+            const productDiv = document.createElement('div');
+            productDiv.innerHTML = `
+                <h2>${product.name}</h2>
+                <p>Price: ${web3.utils.fromWei(product.priceInEth, 'ether')} ETH</p>
+                <p>Quantity: ${product.quantity}</p>
+                <button onclick="addToCart('${product.name}', ${product.priceInEth}, ${product.quantity})">Add to Cart</button>
+            `;
+            productsDiv.appendChild(productDiv);
+        });
+    } else {
+        alert('MetaMask is not installed');
+    }
+}
+
+async function addToCart(productName, priceInEth, quantity) {
+    const contractAddress = '0xCD4c773a5BA577075A3758499F22E5fBD55fDC6A';
+    const contractABI = [/*... ABI from the contract above ...*/];
+    
+    if (typeof window.ethereum !== 'undefined') {
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+        
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        await contract.methods.addToCart(productName, priceInEth, quantity).send({ from: window.ethereum.selectedAddress });
+        alert('Added to cart');
+        fetchCart();
+    } else {
+        alert('MetaMask is not installed');
+    }
+}
+
+async function fetchCart() {
+    const contractAddress = '0xCD4c773a5BA577075A3758499F22E5fBD55fDC6A';
+    const contractABI = [/*... ABI from the contract above ...*/];
+    
+    if (typeof window.ethereum !== 'undefined') {
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+        
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const cart = await contract.methods.getCart().call({ from: window.ethereum.selectedAddress });
+        
+        const cartDiv = document.getElementById('cart');
+        cartDiv.innerHTML = '<h2>Your Cart</h2>';
+        cart.forEach(item => {
+            const itemDiv = document.createElement('div');
+            itemDiv.innerHTML = `
+                <p>${item.name}</p>
+                <p>Price: ${web3.utils.fromWei(item.priceInEth, 'ether')} ETH</p>
+                <p>Quantity: ${item.quantity}</p>
+            `;
+            cartDiv.appendChild(itemDiv);
+        });
+    } else {
+        alert('MetaMask is not installed');
+    }
+}
+
+async function purchase() {
+    const contractAddress = '0xCD4c773a5BA577075A3758499F22E5fBD55fDC6A';
+    const contractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "productName",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "priceInEth",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "quantity",
+				"type": "uint256"
+			}
+		],
+		"name": "addToCart",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "buyKavaPacket",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
 	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
@@ -191,6 +292,123 @@ const contractABI = [
 		"type": "event"
 	},
 	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_location",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_issueType",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_actionTaken",
+				"type": "string"
+			}
+		],
+		"name": "recordGrowthIssue",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_location",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_variety",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "recordHarvest",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_location",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_pestType",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_method",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_cost",
+				"type": "uint256"
+			}
+		],
+		"name": "recordPestControl",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_location",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_variety",
+				"type": "string"
+			}
+		],
+		"name": "recordPlanting",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_price",
+				"type": "uint256"
+			}
+		],
+		"name": "recordSale",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
 		"anonymous": false,
 		"inputs": [
 			{
@@ -218,32 +436,70 @@ const contractABI = [
 	{
 		"inputs": [
 			{
+				"internalType": "address payable",
+				"name": "_sellerAddress",
+				"type": "address"
+			},
+			{
 				"internalType": "string",
-				"name": "productName",
+				"name": "_sellerName",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_sellerContact",
 				"type": "string"
 			},
 			{
 				"internalType": "uint256",
-				"name": "priceInEth",
+				"name": "_quality",
 				"type": "uint256"
 			},
 			{
 				"internalType": "uint256",
-				"name": "quantity",
+				"name": "_quantity",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_price",
 				"type": "uint256"
 			}
 		],
-		"name": "addToCart",
+		"name": "setKavaPacket",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
-		"inputs": [],
-		"name": "buyKavaPacket",
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
 		"outputs": [],
-		"stateMutability": "payable",
+		"stateMutability": "nonpayable",
 		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
 	},
 	{
 		"inputs": [
@@ -504,123 +760,6 @@ const contractABI = [
 	{
 		"inputs": [
 			{
-				"internalType": "string",
-				"name": "_location",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_issueType",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_actionTaken",
-				"type": "string"
-			}
-		],
-		"name": "recordGrowthIssue",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_location",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_variety",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_amount",
-				"type": "uint256"
-			}
-		],
-		"name": "recordHarvest",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_location",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_pestType",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_method",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_cost",
-				"type": "uint256"
-			}
-		],
-		"name": "recordPestControl",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_location",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_variety",
-				"type": "string"
-			}
-		],
-		"name": "recordPlanting",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_amount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_price",
-				"type": "uint256"
-			}
-		],
-		"name": "recordSale",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "renounceOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
 				"internalType": "uint256",
 				"name": "",
 				"type": "uint256"
@@ -646,108 +785,38 @@ const contractABI = [
 		],
 		"stateMutability": "view",
 		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address payable",
-				"name": "_sellerAddress",
-				"type": "address"
-			},
-			{
-				"internalType": "string",
-				"name": "_sellerName",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_sellerContact",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_quality",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_quantity",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_price",
-				"type": "uint256"
-			}
-		],
-		"name": "setKavaPacket",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "newOwner",
-				"type": "address"
-			}
-		],
-		"name": "transferOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_amount",
-				"type": "uint256"
-			}
-		],
-		"name": "withdraw",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"stateMutability": "payable",
-		"type": "receive"
 	}
-
+];
+    
     if (typeof window.ethereum !== 'undefined') {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const contract = new ethers.Contract(contractAddress, contractABI, provider);
-
-        try {
-            const products = await contract.getCart();
-            displayProducts(products);
-        } catch (error) {
-            console.error(error);
-            alert('Failed to fetch products. Please try again.');
-        }
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+        
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const cart = await contract.methods.getCart().call({ from: window.ethereum.selectedAddress });
+        
+        let totalAmount = 0;
+        cart.forEach(item => {
+            totalAmount += item.priceInEth;
+        });
+        
+        await contract.methods.buyKavaPacket().send({ from: window.ethereum.selectedAddress, value: totalAmount });
+        alert('Purchase successful');
+        fetchCart();
     } else {
-        alert('MetaMask is not installed. Please install it to log in.');
+        alert('MetaMask is not installed');
     }
 }
 
-function displayProducts(products) {
-    const productsContainer = document.getElementById('products-container');
-    productsContainer.innerHTML = ''; // Clear existing products
+document.getElementById('connectWallet').addEventListener('click', async () => {
+    if (typeof window.ethereum !== 'undefined') {
+        await window.ethereum.enable();
+        alert('Wallet connected');
+    } else {
+        alert('MetaMask is not installed');
+    }
+});
 
-    products.forEach(product => {
-        const productElement = document.createElement('div');
-        productElement.className = 'product';
-        productElement.innerHTML = `
-            <img src="${product.image}" alt="${product.name}">
-            <h3>${product.name}</h3>
-            <p>Price: ${ethers.utils.formatUnits(product.priceInEth, 'ether')} ETH</p>
-            <p>Quantity: ${product.quantity}</p>
-            <button onclick="addToCart('${product.name}', ${product.priceInEth}, ${product.quantity})">Add to Cart</button>
-        `;
-        productsContainer.appendChild(productElement);
-    });
-}
+document.getElementById('purchase').addEventListener('click', purchase);
 
 window.onload = fetchProducts;
